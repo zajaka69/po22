fbd074a1b270bb67b4df0e1122e8efbce82c114b
дом <zajajaja@yandex.ru>
2026-04-26 18:37:56 +0300

    Убрана ошибка

--- includes/header.php
+++ includes/header.php
@@ -16,10 +16,6 @@ $menuItems = [
                     </a>
                 </li>
         <?php endforeach; ?>
-
-            <li><a href="/index.php">Главная</a></li>
-            <li><a href="/pages/about.php">О нас</a></li>
-            <li><a href="/pages/contact.php">Контакты</a></li>
         </ul>
     </nav>
 </header>

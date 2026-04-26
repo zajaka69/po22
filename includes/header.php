5f76e95672a88a1707ad886796cf7e16c93aec5c
дом <zajajaja@yandex.ru>
2026-04-26 18:13:27 +0300

    Добавлен header.php

---
+++ includes/header.php
@@ -0,0 +1,9 @@
+<header>
+    <nav>
+        <ul>
+            <li><a href="/index.php">Главная</a></li>
+            <li><a href="/pages/about.php">О нас</a></li>
+            <li><a href="/pages/contact.php">Контакты</a></li>
+        </ul>
+    </nav>
+</header>

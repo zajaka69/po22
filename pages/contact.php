49f6ea5b6bf3c927d26e7149cc913cb0262f1da8
дом <zajajaja@yandex.ru>
2026-04-26 18:19:21 +0300

    Добавлен contact.php

---
+++ pages/contact.php
@@ -0,0 +1,11 @@
+<?php include '../includes/header.php'; ?>
+<main>
+    <h1>Контакты</h1>
+    <form method="post">
+        <input type="text" name="name" placeholder="Имя">
+        <input type="email" name="email" placeholder="Email">
+        <textarea name="message"></textarea>
+        <button type="submit">Отправить</button>
+    </form>
+</main>
+<?php include '../includes/footer.php'; ?>

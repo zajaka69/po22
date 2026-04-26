21571c46aeb0a63ed54cc1fdf51ea4102e8045e0
дом <zajajaja@yandex.ru>
2026-04-26 18:23:10 +0300

    Добавлено динамическое меню

--- includes/header.php
+++ includes/header.php
@@ -1,6 +1,22 @@
+<?php
+$menuItems = [
+    'Главная' => '/index.php',
+    'О нас' => '/pages/about.php',
+    'Контакты' => '/pages/contact.php'
+];
+?>
+
+
 <header>
     <nav>
         <ul>
+        <?php foreach ($menuItems as $title => $url): ?>
+             <li><a href="<?= $url ?>" <?= $_SERVER['REQUEST_URI'] == $url ? 'class="active"' : '' ?>>
+                        <?= $title ?>
+                    </a>
+                </li>
+        <?php endforeach; ?>
+
             <li><a href="/index.php">Главная</a></li>
             <li><a href="/pages/about.php">О нас</a></li>
             <li><a href="/pages/contact.php">Контакты</a></li>
